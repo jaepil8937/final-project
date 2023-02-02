12dee0ce8959bf5a5282b31d5329a854487245ea
jaepil8937 <jphan8937@naver.com>
2023-02-02 17:15:59 +0900

    modify

--- src/main/java/com/last/web/contoller/PayController.java
+++ src/main/java/com/last/web/contoller/PayController.java
@@ -2,12 +2,14 @@ package com.last.web.contoller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/pay")
 public class PayController {
 	
 	@GetMapping("/paybank")
 	public String paybank() {
-		return "paybank";
+		return "pay/paybank";
 	}
 }
