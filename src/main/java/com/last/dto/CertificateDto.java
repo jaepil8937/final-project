b2a6309149aff33c2b985d5b26a6d27853421bce
cjy <jun960116@naver.com>
2023-02-16 13:12:27 +0900

    CertificateDto 추가, certificate.jsp 수정, employee.xml에 조회쿼리 추가

---
+++ src/main/java/com/last/dto/CertificateDto.java
@@ -0,0 +1,25 @@
+package com.last.dto;
+
+import java.util.Date;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Alias("CertificateDto")
+@Getter
+@Setter
+public class CertificateDto {
+
+	private String requestNo;		// 신청번호
+	private String type;			// 증명서구분
+	private Date requestDate;		// 신청일자
+	private String no;				// 사원번호
+	private String name;			// 성명
+	private String requestStatus;	// 발행상태
+	private Date publishDate;		// 발행일자
+	private String purpose;			// 용도
+	private String status;			// 재직상태
+	
+}
